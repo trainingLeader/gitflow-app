af917aed978c3a59f2dd935e86bacef42be4f776
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-11-28 19:20:05 -0500

    Add new features to JS and Html

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón Accionado!';
 });
+//Metodo que se ejecuta cuando se presiona un boton
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
+});
